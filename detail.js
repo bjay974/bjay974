3d872c7bced62b0adb480361bcbccb45c9280665
bjay974 <163647044+bjay974@users.noreply.github.com>
2024-03-17 14:47:34 +0100

    Update detail.js

--- detail.js
+++ detail.js
@@ -1,30 +1,3 @@
-window.onload = function() {
-    // Récupérer l'ID de la personne à partir de l'URL
-    const urlParams = new URLSearchParams(window.location.search);
-    const personId = urlParams.get('id');
-
-    // Charger les données JSON
-    fetch('data.json')
-        .then(response => response.json())
-        .then(data => {
-            // Trouver la personne correspondante
-            const person = data.find(p => p.id === parseInt(personId));
-
-            // Afficher les détails de la personne
-            const personDetails = document.getElementById('person-details');
-            if (person) {
-                personDetails.innerHTML = `
-                    <p>Nom : ${person.nom}</p>
-                    <p>Prénom : ${person.prenom}</p>
-                    <p>Date de Naissance  : ${person.date_naissance}</p>
-                `;
-            } else {
-                personDetails.textContent = 'Personne non trouvée';
-            }
-        })
-        .catch(error => console.error('Erreur lors du chargement des données :', error));
-};
-
 document.addEventListener('DOMContentLoaded', () => {
     const personDetails = document.getElementById('person-details');
 
