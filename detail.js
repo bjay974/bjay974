0493268de0760af06b5ea440f5b1a0d3e087373b
bjay974 <163647044+bjay974@users.noreply.github.com>
2024-03-18 16:12:58 +0100

    Update detail.js

--- detail.js
+++ detail.js
@@ -116,14 +116,14 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 function calculateAge(date1, date2) {
-    const annee1 = new Date(date1).getFullYear();
-    const annee2 = new Date(date2).getFullYear();
+    let annee1 = new Date(date1).getFullYear();
+    let annee2 = new Date(date2).getFullYear();
     return annee1 - annee2;
 }
 
 // Obtenir l’âge
 function twCalculeAge(date1) {
-    const annee1 = new Date().getFullYear();
-    const annee2 = new Date(date1).getFullYear();
+    let annee1 = new Date().getFullYear();
+    let annee2 = new Date(date1).getFullYear();
     return annee1 - annee2;
 }
