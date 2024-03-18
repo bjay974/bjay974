5e36fa66eab735e79aa690e58c5ba3f91392ea86
bjay974 <163647044+bjay974@users.noreply.github.com>
2024-03-18 19:08:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,17 +2,43 @@
 fetch('data.json')
     .then(response => response.json())
     .then(data => {
-        const personList = document.getElementById('person-list');
-        // Parcourir chaque personne dans les données
-        data.forEach(person => {
-            // Créer un élément de liste pour chaque personne
+        // Filtrer les personnes par genre
+        const hommes = data.filter(person => person.Genre === 'M');
+        const femmes = data.filter(person => person.Genre === 'F');
+
+        const hommeList = document.getElementById('homme-list');
+        const femmeList = document.getElementById('femme-list');
+        
+        // Titre pour la liste des hommes
+        const hommeTitle = document.createElement('h2');
+        hommeTitle.textContent = 'Hommes';
+        hommeList.appendChild(hommeTitle);
+        
+        // Parcourir chaque homme dans les données
+        hommes.forEach(person => {
+            // Créer un élément de liste pour chaque homme
             const listItem = document.createElement('li');
             const link = document.createElement('a');
             link.textContent = person.nom + ' ' + person.prenom;
             link.href = 'person.html?id=' + person.id;
             listItem.appendChild(link);
-            personList.appendChild(listItem);
+            hommeList.appendChild(listItem);
+        });
+
+        // Titre pour la liste des femmes
+        const femmeTitle = document.createElement('h2');
+        femmeTitle.textContent = 'Femmes';
+        femmeList.appendChild(femmeTitle);
+        
+        // Parcourir chaque femme dans les données
+        femmes.forEach(person => {
+            // Créer un élément de liste pour chaque femme
+            const listItem = document.createElement('li');
+            const link = document.createElement('a');
+            link.textContent = person.nom + ' ' + person.prenom;
+            link.href = 'person.html?id=' + person.id;
+            listItem.appendChild(link);
+            femmeList.appendChild(listItem);
         });
     })
     .catch(error => console.error('Erreur lors du chargement des données :', error));
-
