24abfff970600064bf6705b864505121cb0a56e8
bjay974 <163647044+bjay974@users.noreply.github.com>
2024-03-22 16:30:23 +0100

    Update detail.js

--- detail.js
+++ detail.js
@@ -103,10 +103,11 @@ document.addEventListener('DOMContentLoaded', () => {
                         fatherItem.innerHTML = `<em>${adjectif_genre} de </em><strong>${father.nom} ${father.prenom}</strong>`;
                         detailsList.appendChild(fatherItem);
                     } 
-                } else {
+                 else {
                     const fatherItem = document.createElement('li');
                     fatherItem.innerHTML = `<em>Père </em> inconnu`;
                     detailsList.appendChild(fatherItem);
+                    }
                 }
                 
               // Charger la mère si l'ID de la mère est défini
@@ -120,12 +121,13 @@ document.addEventListener('DOMContentLoaded', () => {
                         detailsList.appendChild(motherItem);
                         detailsList.appendChild(document.createElement('br')); // Ajout d'un espace
                      }
-                } else {
+                 else {
                     const motherItem = document.createElement('li');
                     motherItem.innerHTML = `<em>Mère</em> inconnue`;
                     detailsList.appendChild(motherItem);
                     detailsList.appendChild(document.createElement('br')); // Ajout d'un espace
-                }   
+                    }   
+                }
                 
                 // Récupérer les enfants de la personne si elle est définie comme père ou mère
                 const childrenOfPerson = data.filter(child => child.id_pere === person.id || child.id_mere === person.id);
