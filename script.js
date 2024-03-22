4eaef4590801f48055074c4323a8897219f39e06
bjay974 <163647044+bjay974@users.noreply.github.com>
2024-03-22 15:12:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,20 +3,25 @@ fetch('data.json')
     .then(response => response.json())
     .then(data => {
         // Filtrer les personnes par genre
-        const hommes = data.filter(person => person.genre === 'M');
-        const femmes = data.filter(person => person.genre === 'F');
-
+        const hommes = data.filter(person => person.genre === 'M' && person.id <= 999);
+        const femmes = data.filter(person => person.genre === 'F' && person.id <= 999);
+        const hommesMat = data.filter(person => person.genre === 'M' && person.id >= 1000 && person.id <= 1999); 
+        const femmesMat = data.filter(person => person.genre === 'F' && person.id >= 1000 && person.id <= 1999);
+        
         const hommeList = document.getElementById('homme-list');
         const femmeList = document.getElementById('femme-list');
+        const hommeListMat = document.getElementById('homme-list');
+        const femmeListMat = document.getElementById('femme-list');        
         
         // Titre pour la liste des hommes
         const hommeTitle = document.createElement('h4');
         hommeTitle.style.color = "blue";
         hommeTitle.style.fontStyle ="italic";
-        hommeTitle.textContent = 'Hommes';
+        hommeTitle.textContent = 'Hommes (Branche Paternelle)';
         hommeList.appendChild(hommeTitle);
         
         // Parcourir chaque homme dans les données
+        hommes.sort((a, b) => b.id - a.id);
         hommes.forEach(person => {
             // Créer un élément de liste pour chaque homme
             const listItem = document.createElement('li');
@@ -32,10 +37,11 @@ fetch('data.json')
         const femmeTitle = document.createElement('h4');
         femmeTitle.style.color = "blue";
         femmeTitle.style.fontStyle ="italic";
-        femmeTitle.textContent = 'Femmes';
+        femmeTitle.textContent = 'Femmes (Branche Paternelle)';
         femmeList.appendChild(femmeTitle);
         
         // Parcourir chaque femme dans les données
+        femmes.sort((a, b) => b.id - a.id);
         femmes.forEach(person => {
             // Créer un élément de liste pour chaque femme
             const listItem = document.createElement('li');
@@ -46,5 +52,46 @@ fetch('data.json')
             listItem.appendChild(link);
             femmeList.appendChild(listItem);
         });
+        
+         // Titre pour la liste des hommes coté maternelle
+        const hommeTitleMat = document.createElement('h4');
+        hommeTitleMat.style.color = "blue";
+        hommeTitleMat.style.fontStyle ="italic";
+        hommeTitleMat.textContent = 'Hommes (Branche Maternelle)';
+        hommeListMat.appendChild(hommeTitleMat);
+        
+        // Parcourir chaque homme dans les données
+        hommesMat.sort((a, b) => b.id - a.id);
+        hommesMat.forEach(person => {
+            // Créer un élément de liste pour chaque homme
+            const listItemMat = document.createElement('li');
+            const linkMat = document.createElement('a');
+            linkMat.textContent = person.nom + ' ' + person.prenom;
+            linkMat.href = 'person.html?id=' + person.id;
+            linkMat.classList.add("lien"); //ajout la classe lien du ccs au lien
+            listItemMat.appendChild(linkMat);
+            hommeListMat.appendChild(listItemMat);
+        });
+
+        // Titre pour la liste des femmes coté maternelle
+        const femmeTitleMat = document.createElement('h4');
+        femmeTitleMat.style.color = "blue";
+        femmeTitleMat.style.fontStyle ="italic";
+        femmeTitleMat.textContent = 'Femmes (Branche Maternelle)';
+        femmeListMat.appendChild(femmeTitleMat);
+        
+        // Parcourir chaque femme dans les données
+        femmesMat.sort((a, b) => b.id - a.id);
+        femmesMat.forEach(person => {
+            // Créer un élément de liste pour chaque femme
+            const listItemMat = document.createElement('li');
+            const linkMat = document.createElement('a');
+            linkMat.textContent = person.nom + ' ' + person.prenom;
+            linkMat.href = 'person.html?id=' + person.id;
+            linkMat.classList.add("lien"); //ajout la classe lien du ccs au lien
+            listItemMat.appendChild(linkMat);
+            femmeListMat.appendChild(listItemMat);
+        });
+        
     })
     .catch(error => console.error('Erreur lors du chargement des données :', error));
