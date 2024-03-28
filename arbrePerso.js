ccd2dac07c86ee8546078aed7e5ef9153d0c1934
bjay974 <163647044+bjay974@users.noreply.github.com>
2024-03-28 10:48:52 +0100

    modif

--- arbrePerso.js
+++ arbrePerso.js
@@ -1,65 +1,58 @@
-// Fonction pour récupérer les paramètres de l'URL
-function getUrlParams() {
-    var params = {};
-    window.location.search.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(str,key,value) {
-      params[key] = value;
-    });
-    console.log(params);
-    return params;
-  }
   
 // Fonction pour charger les données JSON
 function loadJSON(callback) {   
-var xobj = new XMLHttpRequest();
-xobj.overrideMimeType("application/json");
-xobj.open('GET', 'data.json', true);
-xobj.onreadystatechange = function () {
-    if (xobj.readyState == 4 && xobj.status == 200) {
-    callback(xobj.responseText);
-    }
-};
-xobj.send(null);  
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'data.json', true);
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == 200) {
+        callback(xobj.responseText);
+        }
+    };
+    xobj.send(null);  
 }
 
 // Fonction pour afficher les données dans la page
 function displayData() {
-var params = getUrlParams();
-var personId = params.id;
-console.log(personId);
 
-loadJSON(function(response) {
-    var data = JSON.parse(response);
-    var person = data.find(person => person.id === personId);
-    var personContainer = document.getElementById('person-container');
-    
-    // Afficher les informations de la personne
-    var genderClass = person.id_genre === 'M' ? 'male' : 'female';
-    var personHTML = '<div class="container ' + genderClass + '">';
-    personHTML += '<h4>' + person.nom + ' ' + person.prenom + '</h4>';
-    personHTML += '<p>Date de naissance : ' + person.date_naissance + '</p>';
-    personHTML += '</div>';
-    personContainer.innerHTML = personHTML;
-    
-    // Afficher les parents
-    displayRelations(person.id_pere, person.id_mere, 'parent', 'Parents', data);
-    
-    // Afficher les grands-parents
-    var father = data.find(person => person.id === person.id_pere);
-    var mother = data.find(person => person.id === person.id_mere);
-    if (father && mother) {
-    displayRelations(father.id_pere, father.id_mere, 'grandparent', 'Grands-parents', data);
-    displayRelations(mother.id_pere, mother.id_mere, 'grandparent', 'Grands-parents', data);
-    }
-    
-    // Afficher les enfants
-    displayChildren(person.id, 'child', 'Enfants', data);
-    
-    // Afficher les petits-enfants
-    var children = data.filter(child => child.id_pere === person.id || child.id_mere === person.id);
-    children.forEach(function(child) {
-    displayChildren(child.id, 'grandchild', 'Petits-enfants', data);
+    var urlParams = new URLSearchParams(window.location.search);
+    var personId = urlParams.get('id');
+
+    console.log(personId);
+
+    loadJSON(function(response) {
+        var data = JSON.parse(response);
+        var person = data.find(person => person.id === personId);
+        var personContainer = document.getElementById('person-container');
+        
+        // Afficher les informations de la personne
+        var genderClass = person.id_genre === 'M' ? 'male' : 'female';
+        var personHTML = '<div class="container ' + genderClass + '">';
+        personHTML += '<h4>' + person.nom + ' ' + person.prenom + '</h4>';
+        personHTML += '<p>Date de naissance : ' + person.date_naissance + '</p>';
+        personHTML += '</div>';
+        personContainer.innerHTML = personHTML;
+        
+        // Afficher les parents
+        displayRelations(person.id_pere, person.id_mere, 'parent', 'Parents', data);
+        
+        // Afficher les grands-parents
+        var father = data.find(person => person.id === person.id_pere);
+        var mother = data.find(person => person.id === person.id_mere);
+        if (father && mother) {
+        displayRelations(father.id_pere, father.id_mere, 'grandparent', 'Grands-parents', data);
+        displayRelations(mother.id_pere, mother.id_mere, 'grandparent', 'Grands-parents', data);
+        }
+        
+        // Afficher les enfants
+        displayChildren(person.id, 'child', 'Enfants', data);
+        
+        // Afficher les petits-enfants
+        var children = data.filter(child => child.id_pere === person.id || child.id_mere === person.id);
+        children.forEach(function(child) {
+        displayChildren(child.id, 'grandchild', 'Petits-enfants', data);
+        });
     });
-});
 }
 
 // Fonction pour afficher les relations (parents, grands-parents)
