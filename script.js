a62f19b7cb399efad59f320fae1404e3994ffc27
bjay974 <163647044+bjay974@users.noreply.github.com>
2024-08-20 09:58:09 +0200

    modif

--- script.js
+++ script.js
@@ -118,10 +118,11 @@ function createListItem(person) {
 function createGenerationId(person) {
     const personId = person.id
     if (personId < 1000) {
-        const idGeneration = parseInt(personId.substr(0,1));
+        const idGeneration = parseInt(personId.substr(0, 1));
+        const an = parseInt(date.substr(6, 4));
     }
     else if (personId >= 1000 && personId < 2000) {
-        const idGeneration = parseInt(personId.substr(0,2));
+        const idGeneration = parseInt(personId.substr(0, 2));
     }
     return idGeneration;
 }
