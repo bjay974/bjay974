a32fe16f24a3cb94ba9edebeec90d9f94095ee53
bjay974 <163647044+bjay974@users.noreply.github.com>
2024-08-20 10:30:30 +0200

    modif

--- script.js
+++ script.js
@@ -97,10 +97,10 @@ function createListItem(person) {
     const listAnNaissance = createAnNaissance(person.date_naissance);  
     const link = document.createElement('a');
     if (person.origine) {
-        link.textContent = person.nom + ' ' + person.prenom + ' (' + person.origine + ')' + ' --- '  + ' (' + listAnNaissance + ') --- G:' + listGeneration;
+        link.textContent = person.nom + ' ' + person.prenom + ' (' + person.origine + ')' + ' - '  + ' (' + listAnNaissance + ') - G ' + listGeneration;
     }
     else {
-        link.textContent = person.nom + ' ' + person.prenom + ' --- ' + ' (' + listAnNaissance + ') --- G:'  + listGeneration ;
+        link.textContent = person.nom + ' ' + person.prenom + ' - ' + ' (' + listAnNaissance + ') - G '  + listGeneration ;
     }
     if (person.id >= 2000) {
      link.href = 'person.html?id=' + person.id;
@@ -131,6 +131,9 @@ function createGenerationId(person) {
 }
 
 function createAnNaissance(date) {
-    const year = parseInt(date.substr(6, 4));
-    return year;
+    const year = parseInt(date.substr(6, 4)); // Extrait l'année à partir de la chaîne de date
+    if (year === 1901) { 
+        return '??'; // Retourne un point d'interrogation pour indiquer une année inconnue
+    }
+    return year; 
 }
