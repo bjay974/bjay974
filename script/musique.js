1ecafa961758cc41c65bbf0c43de0778e7749858
bjay974 <163647044+bjay974@users.noreply.github.com>
2024-09-01 00:42:57 +0200

    modif

--- script/musique.js
+++ script/musique.js
@@ -1,14 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
     var audio = document.getElementById('audioPlayer');
+    var playButton = document.getElementById('playButton');
     audio.volume = 0.8;  // Ajuster le volume
 
     // Définir le temps de départ en secondes
     var startTime = 18;
     
     // Lancer la musique à la position startTime
-    audio.addEventListener('loadedmetadata', function() {
+    playButton.addEventListener('click', function() {
         audio.currentTime = startTime;  // Positionner à 18 secondes
         audio.play();  // Démarrer la lecture
+        playButton.style.display = 'none';  // Cacher le bouton après le démarrage
     });
 });
 
