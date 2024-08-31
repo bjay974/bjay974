9fa1d06cb7d3f7126b0a92153816971914566245
bjay974 <163647044+bjay974@users.noreply.github.com>
2024-08-31 23:38:50 +0200

    valiode

---
+++ script/contact.js
@@ -0,0 +1,37 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Empêche le rechargement de la page
+
+    const serviceID = 'service_s5iw748';  // Mon Service ID
+    const templateID = 'template_74aqzqf'; // Mon Template ID
+    const userID = 'rHp6xKxkYt_JZl33u'; // Mon User ID
+
+    const templateParams = {
+        nom: document.getElementById('nom').value,
+        adresseMail: document.getElementById('adresseMail').value,
+        sujet: document.getElementById('sujet').value,
+        message: document.getElementById('message').value
+    };
+
+    fetch('https://api.emailjs.com/api/v1.0/email/send', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                service_id: serviceID,
+                template_id: templateID,
+                user_id: userID,
+                template_params: templateParams
+            })
+        })
+        .then(response => {
+            alert('Lé Valab. Merci pou out message');
+            window.location.href = 'index.html'; // Redirige vers la page d'accueil après succès
+        })
+        .catch(error => {
+            console.error('FAILED...');
+            alert('L\'envoi du message a échoué, veuillez réessayer.');
+    });
+
+    document.getElementById('contact-form').reset();
+});
