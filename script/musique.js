c4a9d0881ab3977676d9179bf61acb6942513a8a
bjay974 <163647044+bjay974@users.noreply.github.com>
2024-08-31 14:07:46 +0200

    modif

--- script/musique.js
+++ script/musique.js
@@ -1,15 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var audioPlayer = document.getElementById('audioPlayer');
+    var audio = document.getElementById('audioPlayer');
     audioPlayer.volume = 0.8;
     // start de la musique( en secondes)
     var startTime = 18;
     
     // Lance la musique 
-    audioPlayer.addEventListener('loadedmetadata', function() {
+    audio.addEventListener('loadedmetadata', function() {
         audioPlayer.currentTime = startTime;
     });
 
     // lancement auto
-    audioPlayer.play();
-
+    audio.play();
 });
