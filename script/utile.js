0bab6144f8394822d1d1092cade89abc6c3adf4d
bjay974 <163647044+bjay974@users.noreply.github.com>
2024-09-02 18:52:49 +0200

    modif

--- script/utile.js
+++ script/utile.js
@@ -2,7 +2,7 @@ const urlCourrante = window.location.pathname;
 const liensMenu = document.querySelectorAll('nav a');
 
 liensMenu.forEach(lien => {
-    const lienChemin = new URL(lien.href).pathname.split('/')/pop();
+    const lienChemin = new URL(lien.href).pathname.split('/').pop();
     const pageCourrante = urlCourrante.split('/').pop();
     if(lienChemin === pageCourrante) {
         lien.classList.add('lienActif')
