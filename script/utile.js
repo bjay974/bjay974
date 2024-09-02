f443ccc89d08fe0bd2e0e514477530a32c2bf9e3
bjay974 <163647044+bjay974@users.noreply.github.com>
2024-09-02 18:34:46 +0200

    modif

--- script/utile.js
+++ script/utile.js
@@ -2,7 +2,7 @@ const pageCourrante = window.location.pathname;
 const liensMenu = document.querySelectorAll('nav a');
 
 liensMenu.forEach(lien => {
-    if(pageCourrante.includes(lien.getAttribute('href'))) {
+    if(lien.includes(pageCourrante.getAttribute('href'))) {
         lien.classList.add('lienActif')
     }
     
