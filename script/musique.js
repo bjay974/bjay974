296c52063a24556b9e75269059bb8a12904cdb3f
Jérôme BETSMAN <jerome.betsman@laposte.fr>
2024-09-09 11:54:45 +0200

    modif

--- script/musique.js
+++ script/musique.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     var audioPlayer = document.getElementById('audioPlayer');
     audioPlayer.volume = 0.5; // Réglez le volume initial (0 à 1)
-
     // Vous pouvez ajouter d'autres fonctionnalités JavaScript ici, par exemple contrôler le lecteur audio avec des boutons.
 });
 
