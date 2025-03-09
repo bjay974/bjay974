b31a8196439be1786d859084cc72e8554fdef1d8
bjay974 <bjay974@yahoo.fr>
2025-03-09 14:04:38 +0100

    modif

--- script/recap.js
+++ script/recap.js
@@ -21,14 +21,14 @@ fetch('../data/data.json')
         } else {
           femPat.push(person);
         }
-      } else if (person.id >= 1600 && person.id <= 1699) {
+      } else if (person.id >= 1700 && person.id <= 1799) {
         if (person.genre === 'M') {
           homMat.push(person);
         } else {
           femMat.push(person);
         }
       }
-      else if (person.id >= 1700 && person.id <= 1999) {
+      else if (person.id >= 1800 && person.id <= 1999) {
         if (person.genre === 'M') {
           homGen.push(person);
         } else {
